9a63fe3092b3daecea37d147387d8efa666ceda9
SyrettD <134374583+SyrettD@users.noreply.github.com>
2023-08-31 18:07:26 +0100

    Update streamlit_app.py
    added text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # normalise fruityvice data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put fruityvice data in dataframe
