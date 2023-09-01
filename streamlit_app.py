63d4430c413786a9e08c5227d258df9fb083c528
SyrettD <134374583+SyrettD@users.noreply.github.com>
2023-09-01 11:05:48 +0100

    Update streamlit_app.py
    text to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 
 streamlit.header("The fruit load list contains:")
-streamlit.text(my_data_rows)
+streamlit.dataframe(my_data_rows)
